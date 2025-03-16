cbf8fc21aefa6d3efbb982cbfabd578d9da35b38
Alexandra Cordeiro Campos <srcoelho007@gmail.com>
2025-03-16 17:53:48 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/quemsomos')
+def home():
+    return render_template("quemsomos.html")
+
+@app.route('/tratamentos')
+def tratamentos():
+    return render_template("tratamentos.html")
+
+@app.route('/antes-depois')
+def antes_depois():
+    return render_template("antesdepois.html")
+
+@app.route('/novidades')
+def novidades():
+    return render_template("novidades.html")
+
+@app.route('/agenda')
+def agenda():
+    return render_template("agenda.html")
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
