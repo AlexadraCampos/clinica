c5bb53720dae2737570259e1fa96ab1251523931
Alexandra Cordeiro Campos <168585466+AlexadraCampos@users.noreply.github.com>
2025-04-08 07:56:37 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template
-from flask_cors import CORS # 📌 Importando Flask-CORS
 
 app = Flask(__name__)
 
@@ -7,10 +6,6 @@ app.config.update(
     SESSION_COOKIE_SAMESITE='None',  # Permite cookies de terceiros
     SESSION_COOKIE_SECURE=True  
 )
-
-# 🔹 Configurar CORS para permitir requisições do frontend
-CORS(app, origins=["https://ecommercemodelosite-7tztgdygq-alexandras-projects-d68fae16.vercel.app"])
-
 
 @app.route('/')
 @app.route('/quemsomos')
@@ -33,11 +28,5 @@ def novidades():
 def agenda():
     return render_template("agenda.html")
 
-# Função compatível com Vercel
-def handler(request, *args, **kwargs):
-    return app(request.environ, *args, **kwargs)  # Essa linha permite que o Flask funcione no Vercel
-
-# Para evitar app.run(), que não é necessário no Vercel
-# O Vercel já gerencia a execução do Flask por trás
 if __name__ == "__main__":
-    app.run(debug=True)  # Remover se rodar no Vercel, mas pode ficar no local para desenvolvimento local
+    app.run(debug=True)  
